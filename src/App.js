e76a9dad6808c1dc67d1b997f258b90c4ce1fbf8
arif <9104011907000004>
2023-08-10 22:32:16 +0700

    fix<structure>: - fix folder structure - fix path - create welcome, notelist, & userslist page

--- src/App.js
+++ src/App.js
@@ -1,19 +1,36 @@
 import { Route, Routes } from "react-router-dom";
 import Layout from "./components/Layout";
 import Public from "./components/Public";
-import Login from "./components/Login";
+import Login from "./features/auth/Login";
 import DashLayout from "./components/DashLayout";
+import Welcome from "./features/auth/Welcome";
+import NoteList from "./features/notes/NoteList";
+import UsersList from "./features/users/UsersList";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={ <Layout /> }>
+        {/* public routes */}
         <Route index element={ <Public /> } />
-        <Route path="/login" element={ <Login /> } />
+        <Route path="login" element={ <Login /> } />
+        {/* end public routes */}
 
-        <Route path="/dash" element={ <DashLayout /> } >
-          
+        {/* protected routes */}
+        <Route path="dash" element={ <DashLayout /> } >
+
+          <Route index element={ <Welcome/> } />
+
+          <Route path="notes">
+            <Route index element={ <NoteList /> } />
+          </Route>
+
+          <Route path="users">
+            <Route index element={ <UsersList /> } />
+          </Route>
+
         </Route>
+        {/* end protected routes */}
       </Route>
     </Routes>
   )
