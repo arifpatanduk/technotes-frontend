926810ab581b3149fb6d3a5954bb02d4dd6f8af2
arif <9104011907000004>
2023-08-11 15:09:27 +0700

    feat<users, notes>: create component, apislice, and list of the notes & users

--- src/features/notes/NoteList.js
+++ src/features/notes/NoteList.js
@@ -1,9 +1,54 @@
-import React from 'react'
+import { useGetNotesQuery } from './notesApiSlice'
+import Note from './Note'
 
-const NoteList = () => {
-  return (
-    <h1>NoteList</h1>
-  )
+const NotesList = () => {
+  const {
+    data: notes,
+    isLoading,
+    isSuccess, 
+    isError,
+    error
+  } = useGetNotesQuery()
+
+  let content
+
+  // loading state
+  if (isLoading) content = <p>Loading...</p>
+
+  // if error
+  if (isError) {
+    content = <p className="errmsg">{error?.data?.message}</p>
+  }
+
+  // if success
+  if (isSuccess) {
+    const { ids } = notes
+    const tableContent = ids?.length
+    ? ids.map(noteId => <Note key={noteId} noteId={noteId} />)
+    : null
+
+    content = (
+      <table className='table table--notes'>
+        <thead className="table__thead">
+            <tr>
+                <th scope="col" className="table__th note__status">Status</th>
+                <th scope="col" className="table__th note__created">Created</th>
+                <th scope="col" className="table__th note__updated">Updated</th>
+                <th scope="col" className="table__th note__title">Title</th>
+                <th scope="col" className="table__th note__username">Owner</th>
+                <th scope="col" className="table__th note__edit">Edit</th>
+            </tr>
+        </thead>
+        <tbody>
+          {tableContent}
+        </tbody>
+
+      </table>
+    )
+  }
+
+
+  return content
 }
 
-export default NoteList
+export default NotesList
